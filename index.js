cd84b8f0495aaf90878654b7fdd157e473e5347c
Tenzin Tenkyong <ttenkyong@gmail.com>
2021-10-05 21:13:35 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat() {
+    return cats.push("Ralph");
+}
+
+function destructivelyPrependCat() {
+    return cats.unshift("Bob");
+}
+
+function destructivelyRemoveLastCat() {
+    return cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    return cats.shift();
+}
+
+function appendCat(coolCats) {
+    return coolCats = [...cats, "Broom"];
+}
+
+function prependCat(coolCats) {
+    return coolCats = ["Arnold", ...cats]
+}
+
+function removeLastCat() {
+    return cats.slice(0, -1);
+}
+
+function removeFirstCat() {
+    return cats.slice(1);
+}
